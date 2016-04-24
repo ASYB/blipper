9617d13d4f19257538d3f37820e872ee499fbf7a
shaik <shaik.younus2@target.com>
2016-04-24 14:08:30 +0530

    reverse string and prime numers test

---
+++ reverse.string_spec.js
@@ -0,0 +1,17 @@
+/**
+ * Created by z001hgg on 4/24/16.
+ */
+var frisby = require('frisby');
+var strings_to_reverse = ["shaik","shaik younus","Shaik Younus","shaik.younus2@target.com","YOUNUS","123 432"," ","123"]
+
+for(var i = 0; i<strings_to_reverse.length;i++ ) {
+
+    frisby.create('Reverse a string')
+        .post('http://findthebug.herokuapp.com/reversewords?',{
+            string:strings_to_reverse[i]
+        })
+        .expectBodyContains(
+            strings_to_reverse[i].split("").reverse().join("").split(" ").reverse().join(" ")
+        )
+        .toss();
+}
